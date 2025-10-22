4f8725966bfe5ce52cb8d35cc2ea26b770c55e24
sdemchenko <sdemchenko@gmail.com>
2025-10-22 14:15:53 +0300

    Revert "Implemented collapsible sections experiment."
    This reverts commit 83a6863c2d14d2304d8e6ee82bdbc4ec7a8f6f2b.

--- assets/home.js
+++ assets/home.js
@@ -8,7 +8,6 @@ $(document).ready(function () {
     configurePlaceNameLinksToOpenPlaceInfoOverlay();
     fetchSchedule();
     setInterval(fetchSchedule, 10 * 60 * 1000);
-    restoreCollapsingDetails();
     incrementCounter();
 });
 
@@ -282,21 +281,6 @@ function incrementCounter() {
         }).catch((err) => {
             // Ignore errors — we don’t need a response
             console.warn('Counter fetch failed silently:', err);
-        });
-    }
-}
-
-function restoreCollapsingDetails() {
-    for (const details of document.querySelectorAll('details[id]')) {
-        const key = 'detailsState_' + details.id;
-        const state = localStorage.getItem(key);
-        if (state === 'open') {
-            details.open = true;
-        } else if (state === 'closed') {
-            details.open = false;
-        }
-        details.addEventListener('toggle', () => {
-            localStorage.setItem(key, details.open ? 'open' : 'closed');
         });
     }
 }
